0caeaa4fcbfc9973c8c26001b4fdb79fe5cbaa0b
ownport <ownport@gmail.com>
2021-03-20 11:54:07 +0200

    refact: compile option

--- scripts/manage.sh
+++ scripts/manage.sh
@@ -20,9 +20,20 @@ function help() {
 
 function compile() {
 
-    echo "[INFO] Compiling code to zip package" && \
-    mkdir -p $(pwd)/target
-    cd $(pwd)/src/; zip --quiet -r ../target/${PROJECT_NAME_BIN} *
+    CURRENT_DIR=$(pwd)
+    echo "[INFO] Compiling code to zip package, ${CURRENT_DIR}" && \
+
+    echo "[INFO] Cleanup ${CURRENT_DIR%%/}/src/ directory before compile" && \
+        [ -d "${CURRENT_DIR}/src/filetools/__pycache__" ] && \
+            rm -rf ${CURRENT_DIR}/src/filetools/__pycache__
+        [ -d "${CURRENT_DIR}/src/filetools/filetools.egg-info" ] && \
+            rm -rf ${CURRENT_DIR}/src/filetools/filetools.egg-info
+
+    mkdir -p $(pwd)/target && \
+        rm -rf ${pwd}/target/*
+    
+    cd $(pwd)/src && zip -r ../target/${PROJECT_NAME_BIN} * && cd -
+    
     echo "#!${PYTHON}" > target/${PROJECT_NAME_BIN} && \
         cat target/${PROJECT_NAME_BIN}.zip >> target/${PROJECT_NAME_BIN} && \
         rm target/${PROJECT_NAME_BIN}.zip && \
