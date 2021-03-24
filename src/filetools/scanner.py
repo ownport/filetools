67744876c0ca4ff5ba36830f9f51cf750252f4f4
ownport <ownport@gmail.com>
2021-03-24 09:04:06 +0200

    refact: use progress bar instead of log messages for scan command

--- src/filetools/scanner.py
+++ src/filetools/scanner.py
@@ -2,12 +2,17 @@
 import os
 import json
 import logging
+import pathlib
+
+from collections import Counter
 
 from filetools.formats import sqlite
 from filetools.formats import jsonline
 
 from filetools.utils import get_meta
 from filetools.utils import scan_files
+from filetools.utils import progress_bar
+from filetools.libs.tabulate import tabulate
 
 logger = logging.getLogger(__name__)
 
@@ -32,40 +37,46 @@ class Scanner:
     def scan_files(self):
         ''' run scanner for getting files metadata
         '''
+        processed_files = 0
+        total_files = self.stats().get('total files')
+        
         try:
-            total_files = 0
+            progress_bar(processed_files, total_files)
             for filepath in scan_files(self._path):
-                total_files += 1
+                processed_files += 1
                 meta = get_meta(filepath, ignore_tags=self._ignore_tags)
                 meta['tags'] = json.dumps(meta['tags'])
                 self._metastore.put(meta)
-                if total_files % 1000 == 0:
-                    logger.info("Processed {} files".format(total_files))
+                if processed_files % 100 == 0:
+                    progress_bar(processed_files, total_files)
                     self._metastore.commit()
             self._metastore.commit()
-            logger.info('Total processed files: {}'.format(total_files))
+            print()
+            print(tabulate((
+                    ('Processed files', processed_files),
+                    ('Total files', total_files)
+                ),tablefmt='github')
+            )
         except KeyboardInterrupt:
             print("Interrupted by user")
 
     def stats(self):
-        ''' returns statistics for path
+        ''' scan directory for gettings statistics
+        returns the next metrics:
+        - total files
+        - total directories
+        - total size
         '''
         try:
-            total_files = 0
-            total_directories = 0
-            for filepath in scan_path(self._path):
-                total_files += 1
-                meta = get_meta(filepath, ignore_tags=self._ignore_tags)
-                meta['tags'] = json.dumps(meta['tags'])
-                self._metastore.put(meta)
-                if total_files % 1000 == 0:
-                    logger.info("Processed {} files".format(total_files))
-                    self._metastore.commit()
-            self._metastore.commit()
-            logger.info('Total processed files: {}'.format(total_files))
+            metrics = Counter()
+            for root, _, files in os.walk(self._path):
+                metrics['total files'] += len(files)
+                metrics['total directories'] += 1
+                for filename in files:
+                    metrics['total size'] += pathlib.Path(os.path.join(root, filename)).stat().st_size
+            return dict(metrics)
         except KeyboardInterrupt:
             print("Interrupted by user")
-
 
     def close(self):
         ''' complete work with scanner
